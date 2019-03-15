69e5b78bef46926390de535fc1f94f3c76990c50
EC2 Default User <ec2-user@ip-172-31-47-127.eu-west-1.compute.internal>
2019-03-15 21:44:11 +0000

    golfstore 2nd  commit

--- golfstore/config/routes.rb
+++ golfstore/config/routes.rb
@@ -1,9 +1,51 @@
 Rails.application.routes.draw do
+  get 'orderitems/index'
+  get 'orderitems/show'
+  get 'orderitems/new'
+  get 'orderitems/edit'
+  
+  
+  
+  
+  
+  resources :orders do  
+      resources:orderitems
+    end
+  
+  
+  
+  
+  
+  
+  devise_for :users do
+      resources:orders
+  end
+  
+  get '/checkout' => 'cart#createOrder'
+  
+  
+  
+  
+  
+  
+  
+  get 'cart/index'
+  
+  
+  
   resources :items
   root 'static_pages#home'
-  
   get '/help' =>'static_pages#help'
   get '/about' =>'static_pages#about'
+  
+  get '/login' =>'user#login'
+  get '/logout' =>'user#logout'
+  
+  get '/cart/clear' => 'cart#clearCart'
+  get '/cart/:id' => 'cart#add'  # => is the same as using  , to
+  get '/cart/remove/:id' => 'cart#remove'
+  get '/cart' => 'cart#index'
+  get '/cart/decrease/:id' => 'cart#decrease'
   
   #get 'static_pages/home'
  # get 'static_pages/help'
